2cdf404bef61d0faffa2f221ac8a588516c62f52
Arun Annamalai <36392755+arun-annamalai@users.noreply.github.com>
2021-06-03 22:49:09 -0400

    reverting some changes and merging

--- travelengine/attractions/attractions.py
+++ travelengine/attractions/attractions.py
@@ -4,18 +4,35 @@ from travelengine.attractions.geocoding import get_lat_lon
 import json
 
 
-def get_attractions(place = "Barcelona, Spain"):
-    amadeus = Client(
-        client_id='GoSZ8yaI32CKz3zPPEYbCXuYk0pGXDYM',
-        client_secret='xahrteSDFmMzmbEv'
-    )
+amadeus = Client(
+    client_id='GoSZ8yaI32CKz3zPPEYbCXuYk0pGXDYM',
+    client_secret='xahrteSDFmMzmbEv'
+)
 
-    try:
-        lat, lon = get_lat_lon(place)
-        response = amadeus.reference_data.locations.points_of_interest.get(latitude=lat, longitude= lon)
-        print(response.data)
-    except ResponseError as error:
-        print(error)
+def get_attractions(place):
+    # Get list of attractions in place
+    # place = "Barcelona, Spain"
+    lat, lon = get_lat_lon(place)
+    response = amadeus.reference_data.locations.points_of_interest.get(latitude=lat, longitude= lon)
+    list_of_attractions = []
+    for item in response.data:
+        list_of_attractions.append(item["name"])
+    print(list_of_attractions)
+
+    #find lat,long of each attraction in list
+    attraction_lat_lon_list = []
+
+    # this is a very slow function
+    for attraction in list_of_attractions:
+        lat, lon = get_lat_lon(attraction)
+        attraction_lat_lon_list.append(
+            {
+                "attraction": attraction,
+                "lat": lat,
+                "lon": lon,
+            }
+        )
+    print(attraction_lat_lon_list)
 
 
 def get_attractions2(place):
